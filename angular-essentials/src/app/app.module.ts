3fd18d5567a9bb5eaed36cd19e6fe33766518f95
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-02-21 19:31:50 +0100

    Add custom components

--- angular-essentials/src/app/app.module.ts
+++ angular-essentials/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user.components';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
